e87c6b1da6b8a8b6866f28b182e0d8cae7284553
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-09-05 09:12:49 -0300

    aula 07, 13:21

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
-import Category from "./components/Category";
+import Category, {categories, filterCategory} from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/videos.json";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Meteorologia, Vegetação",
-  "Geologia e Hidrografia"
-]
-
-function filterCategory(id){
-  return videos.filter ( video => video.category === categories[id] )
-}
 
 function App() {
   return (
-    <>
+    <>''
       <Header/>
       <Banner image="home"/>
 
       {/* colocou os filhos dentro das tags (componente com os parametros)*/}
       <Container>
-      {/* <h2>Séries</h2>
-
-        <section className="cards">
-          
-          {
-            // esta mapeamendo e chamando cada um de video e dps cria um car pra cada video e chamando o id
-            videos.map(video => {
-              return <Card id={video.id} key = {video.id}/>
-            })
-          }
-
-        </section> */}
-
-        <Category category = "Geografia">
-          { filterCategory(0).map((video) => <Card id={video.id} key = {video.id}/> )}
-        </Category>
-
-
+        
+        { categories.map((category, index) => 
+          <Category category = {category}>
+            {/* esta mapeamendo e chamando cada um de video e dps cria um car pra cada video e chamando o id */}
+              { filterCategory(index).map((video) => <Card id={video.id} key = {video.id}/> )}
+          </Category>  
+        )}
 
       </Container>
 
